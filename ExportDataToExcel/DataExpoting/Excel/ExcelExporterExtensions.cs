f8569fcbec9c13474f295603a751fb1760dce871
Viktor Kostiuk <viktor.kostiuk@outlook.com>
2020-04-18 16:14:35 +0300

    Add ExcelExporterExtensions

---
+++ ExportDataToExcel/DataExpoting/Excel/ExcelExporterExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ExportDataToExcel.DataExpoting.Excel
+{
+    public static class ExcelExporterExtensions
+    {
+
+        public static void ExportToFile<T>(this IExcelExporter excelExporter, IEnumerable<T> items, string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException($"{nameof(fileName)} is requried", nameof(fileName));
+            }
+
+            using (FileStream fs = File.OpenWrite(fileName))
+            {
+                excelExporter.ExportToSteam(items, fs);
+            }
+        }
+
+        public static void ExportToSteam<T>(this IExcelExporter excelExporter, IEnumerable<T> items, Stream stream)
+        {
+            ModelMetadataBuilder<T> modelMetadataBuilder = new ModelMetadataBuilder<T>();
+            modelMetadataBuilder.InitFromProperties();
+
+            ExportSheetInfo exportSheetInfo = new ExportSheetInfo
+            {
+                Header = typeof(T).Name,
+                Items = items,
+                Metadata = modelMetadataBuilder.Metadata
+            };
+
+            excelExporter.ExportToStream(new[] { exportSheetInfo }, stream);
+        }
+
+    }
+}
